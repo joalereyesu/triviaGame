430c2a200d9684a889f2ada10c80ab63b73d39fb
joalereyesu <61555348+joalereyesu@users.noreply.github.com>
2022-02-09 16:55:00 -0600

    json updated

--- trivia.js
+++ trivia.js
@@ -1,8 +1,36 @@
 var db = require('./db.json');
+var allQuestions = db.Questions;
+var allChoices = db.Choices;
 
 function loadQuestions(){
-    var fiveQs = [];
-    for (num in (0,5)) {
-        
+    var fiveQsIndex = [];
+    for (let i = 0; i<5; i++) {
+        fiveQsIndex[i] = Math.floor(Math.random() * 20);
     }
+    var fiveQs = [];
+    var fiveCs = [];
+    for (let i = 0; i < 5; i++){
+        fiveQs[i] = allQuestions[fiveQsIndex[i]];
+        fiveCs[i] = allChoices[fiveQsIndex[i]];
+    }
+    let questions = document.getElementById('question');
+    let choices = document.getElementById('choices');
+    fiveQs.forEach(element => {
+        let p = document.createElement("p");
+        p.innerHTML = element;
+        questions.appendChild(p);
+        fiveCs.forEach(key, values => {
+            let li = document.createElement("li");
+            li.innerHTML = key;
+            choices.appendChild(li);
+            console.log(key);
+        });
+    });
 }
+
+loadQuestions()
+
+
+
+
+
